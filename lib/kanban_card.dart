7a86011eb3835659da1b72772e3236ed2b3ae220
Gabriel Slater <68284914+gabrielslater@users.noreply.github.com>
2022-11-09 10:11:22 -0500

    Make KanbanCardModel immutable

--- lib/kanban_card.dart
+++ lib/kanban_card.dart
@@ -1,9 +1,25 @@
 class KanbanCardModel {
-  String title;
-  String body;
+  final String _title;
+  final String _body;
+  final int _id;
 
-  KanbanCardModel({
-    required this.title,
-    required this.body,
-  });
+  const KanbanCardModel(this._title, this._body, this._id);
+
+  String get title => _title;
+
+  String get body => _body;
+
+  int get id => _id;
+
+  KanbanCardModel updateTitle(String title) {
+    return KanbanCardModel(title, _body, _id);
+  }
+
+  KanbanCardModel updateBody(String body) {
+    return KanbanCardModel(_title, body, _id);
+  }
+
+  KanbanCardModel copy() {
+    return KanbanCardModel(_title, _body, _id);
+  }
 }
