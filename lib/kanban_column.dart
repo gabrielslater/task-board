ffc7771961a4a5537a8d234adf2a4ba588078fbf
Gabriel Slater <68284914+gabrielslater@users.noreply.github.com>
2022-11-09 23:10:43 -0500

    Rename (element)s to (card)s in searches

--- lib/kanban_column.dart
+++ lib/kanban_column.dart
@@ -18,27 +18,32 @@ class KanbanColumnModel {
 
   void addNewCard(String title, String body, int id) {
     // TODO: ask about throwing errors/exceptions
-    if (_cards.indexWhere((element) => element.id == id) == -1) {
+    if (_cards.indexWhere((card) => card.id == id) == -1) {
       _cards.add(KanbanCardModel(title, body, id));
     }
   }
 
   void addCard(KanbanCardModel card) {
-    if (_cards.indexWhere((element) => element.id == card.id) == -1) {
+    if (_cards.indexWhere((card) => card.id == card.id) == -1) {
       _cards.add(card);
     }
   }
 
   KanbanCardModel getCardById(int id) {
-    return _cards.firstWhere((element) => element.id == id);
+    return _cards.firstWhere((card) => card.id == id);
   }
 
   KanbanCardModel removeCardById(int id) {
-    var idx = _cards.indexWhere((element) => element.id == id);
+    var idx = _cards.indexWhere((card) => card.id == id);
     var card = _cards[idx];
 
     _cards.removeAt(idx);
 
     return card;
+  }
+
+  @override
+  String toString() {
+    return 'KanbanColumnModel{_title: $_title, _cards: $_cards}';
   }
 }
