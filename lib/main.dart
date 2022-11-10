3875e349112eb60ddca553bda7353fb37abc5241
Gabriel Slater <68284914+gabrielslater@users.noreply.github.com>
2022-11-09 21:10:30 -0500

    Revert "Update KanbanCard with immutable copy methods"
    This reverts commit 36175c992c7c292a0c3c36fe9320c51e13a3a5b1.

--- lib/main.dart
+++ lib/main.dart
@@ -151,16 +151,16 @@ class KanbanColumn extends StatelessWidget {
 
 /// Creates a widget for displaying a [KanbanCardModel].
 class KanbanCard extends StatefulWidget {
-  KanbanCardModel _card = const KanbanCardModel("Title", "Body", 0);
+  KanbanCardModel _card = KanbanCardModel(title: "Title", body: "Body");
 
   KanbanCard({Key? key}) : super(key: key);
 
   void editTitle(String title) {
-    _card = _card.updateTitle(title);
+    _card = KanbanCardModel(title: title, body: _card.body);
   }
 
   void editBody(String body) {
-    _card = _card.updateBody(body);
+    _card = KanbanCardModel(title: _card.title, body: body);
   }
 
   @override
