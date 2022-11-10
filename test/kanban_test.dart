7fb11b0c0e5f5ab63faf6934fd6c5457029cb79c
Gabriel Slater <68284914+gabrielslater@users.noreply.github.com>
2022-11-09 21:43:51 -0500

    Rename test file to cluster tests for the future

---
+++ test/kanban_test.dart
@@ -0,0 +1,36 @@
+import 'package:final_project_kanban_board/kanban_card.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Card Tests', () {
+    test('updateTitle makes a deep copy and changes the card\'s title', () {
+      var card = const KanbanCardModel('Title', 'Body', 0);
+      var modifiedCard = card.updateTitle('New Title');
+
+      expect(modifiedCard, isNot(card));
+      expect(modifiedCard.title, equals('New Title'));
+      expect(modifiedCard.body, equals(card.body));
+      expect(modifiedCard.id, equals(card.id));
+    });
+
+    test('updateBody makes a deep copy and changes the card\'s body', () {
+      var card = const KanbanCardModel('Title', 'Body', 0);
+      var modifiedCard = card.updateBody('New Body');
+
+      expect(modifiedCard, isNot(card));
+      expect(modifiedCard.title, equals(card.title));
+      expect(modifiedCard.body, equals('New Body'));
+      expect(modifiedCard.id, equals(card.id));
+    });
+
+    test('Copy makes a deep copy and changes nothing', () {
+      var card = const KanbanCardModel('Title', 'Body', 0);
+      var modifiedCard = card.copy();
+
+      expect(modifiedCard, isNot(card));
+      expect(modifiedCard.title, equals(card.title));
+      expect(modifiedCard.body, equals(card.body));
+      expect(modifiedCard.id, equals(card.id));
+    });
+  });
+}
