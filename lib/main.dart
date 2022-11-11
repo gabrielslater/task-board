042f2bc9cc4501ae0a2c683ec764949ec4591f23
Gabriel Slater <68284914+gabrielslater@users.noreply.github.com>
2022-11-10 19:44:05 -0500

    Move KanbanCard into main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:final_project_kanban_board/kanban_board.dart';
 import 'package:flutter/material.dart';
 
-import 'editable_card_list.dart';
-
 void main() => runApp(const KanbanApp());
 
 class KanbanApp extends StatelessWidget {
@@ -134,5 +132,131 @@ class _KanbanMainPageState extends State<KanbanMainPage> {
         ],
       ),
     );
+  }
+}
+
+class KanbanCard extends StatefulWidget {
+  final String title;
+  final String body;
+  final int id;
+
+  final Function(String title, String body, int id) onEdit;
+  final Function(int id) onDelete;
+
+  const KanbanCard({
+    Key? key,
+    required this.title,
+    required this.body,
+    required this.id,
+    required this.onEdit,
+    required this.onDelete,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _KanbanCardState();
+}
+
+class _KanbanCardState extends State<KanbanCard> {
+  bool _isEditingText = false;
+  late TextEditingController _titleEditingController;
+  late TextEditingController _bodyEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleEditingController = TextEditingController(text: widget.title);
+    _bodyEditingController = TextEditingController(text: widget.body);
+  }
+
+  @override
+  void dispose() {
+    _titleEditingController.dispose();
+    _bodyEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      margin: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+      padding: const EdgeInsets.fromLTRB(16, 4, 16, 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              title,
+              const Spacer(),
+              _buildEditButton(),
+              IconButton(
+                onPressed: onDelete,
+                icon: const Icon(Icons.delete),
+                color: Colors.red,
+              ),
+            ],
+          ),
+          body,
+        ],
+      ),
+    );
+  }
+
+  Widget get title {
+    if (_isEditingText) {
+      return ConstrainedBox(
+          constraints: const BoxConstraints.tightFor(width: 200),
+          child: TextField(controller: _titleEditingController));
+    } else {
+      return Text(
+        widget.title,
+        style: const TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+        ),
+      );
+    }
+  }
+
+  Widget get body {
+    if (_isEditingText) {
+      return TextField(controller: _bodyEditingController);
+    } else {
+      return Text(widget.body);
+    }
+  }
+
+  Widget _buildEditButton() {
+    if (_isEditingText) {
+      return IconButton(
+        onPressed: () {
+          setState(() {
+            widget.onEdit(
+              _titleEditingController.text,
+              _bodyEditingController.text,
+              widget.id,
+            );
+            _isEditingText = false;
+          });
+        },
+        icon: const Icon(Icons.check),
+        color: Colors.green,
+      );
+    } else {
+      return IconButton(
+        onPressed: () {
+          setState(() {
+            _isEditingText = true;
+          });
+        },
+        icon: const Icon(Icons.edit),
+        color: Colors.lightBlue,
+      );
+    }
+  }
+
+  void onDelete() {
+    widget.onDelete(widget.id);
   }
 }
