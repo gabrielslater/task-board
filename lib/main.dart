c358a71b632c425bc17e360dc01c048c78d750c4
Gabriel Slater <68284914+gabrielslater@users.noreply.github.com>
2022-12-08 17:49:38 -0500

    Add tabbed tutorial page.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 
 import 'package:final_project_kanban_board/local_storage.dart';
 import 'package:final_project_kanban_board/task_board_model.dart';
+import 'dart:ui';
+
+import 'package:final_project_kanban_board/kanban_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -564,6 +567,132 @@ class TaskBoardNavigationDrawer extends StatelessWidget {
 
     return Drawer(
       child: drawerItems,
+    );
+  }
+}
+
+class TaskBoardTutorialPage extends StatelessWidget {
+  final String title;
+
+  const TaskBoardTutorialPage({Key? key, required this.title})
+      : super(key: key);
+
+  Widget _buildPage(List<Widget> children) {
+    return SizedBox.expand(
+      child: FractionallySizedBox(
+        widthFactor: 0.6,
+        child: Container(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: children,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const paragraphStyle = TextStyle(fontSize: 16);
+
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 3,
+      child: Scaffold(
+        body: TabBarView(
+          children: [
+            _buildPage([
+              const Text(
+                'Unfinished tasks tend to weigh on our minds. Writing'
+                ' down plans helps eliminate the stress of '
+                'unfinished tasks. It makes tasks feel easier. '
+                'After all, "write an essay" is a much larger '
+                'sounding task than "write a topic statement."',
+                style: paragraphStyle,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Dragging your feet on tasks that seem hard is normal. '
+                'Breaking down tasks helps get around that idea of a '
+                'mountain. The tinier you can make tasks, the easier it '
+                'is to start. Often, once you\'ve started a task, it\'s '
+                'much easier to keep working. "Write one sentence" can '
+                'quickly become the urge to write a whole paragraph '
+                'once started.',
+                style: const TextStyle(fontSize: 16),
+              ),
+            ]),
+            _buildPage([
+              const Text(
+                'A task should be a single, actionable step. It\'s okay if you '
+                'find a task you wrote isn\'t one step, you can always break it '
+                'into more pieces. For example, an essay can often be broken '
+                'down into an introduction, a body, and a conclusion.',
+                style: paragraphStyle,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Cleaning the house is many smaller tasks, like "Clean the '
+                'sink" or "Put the laundry in the wash." Try writing down a '
+                'task you\'ve been putting off and seeing if you can break it '
+                'into smaller pieces. They can be as small as you want. '
+                'Emailing someone can start with "Open my mail client." '
+                'There\'s no one way of doing it. If "Write email" is a '
+                'specific enough task for you, then it\'s fine to write that '
+                'down.',
+                style: paragraphStyle,
+              ),
+            ]),
+            _buildPage([
+              const Text(
+                'Most of the time, there\'s going to be tasks that are more '
+                'urgent than others in a project. There also may be time '
+                'sensitive tasks, or tasks that have to be finished before '
+                'another. "Buy groceries" should probably happen after "Clean '
+                'out old food from the fridge," for example. Tasks will not '
+                'all be accomplishable at once, and many projects take longer '
+                'than a day.',
+                style: paragraphStyle,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Arranging tasks by time required helps you pick '
+                'priorities. If a task seems to require so much time you\'re '
+                'afraid of doing it, it\'s probably too big to be one task. '
+                'See if you can identify parts you can break it into. "Read '
+                'chapters 6-8" can be broken down into reading chapter 6, '
+                'reading chapter 7, and reading chapter 8. It could be broken '
+                'down even further by page numbers if necessary.',
+                style: paragraphStyle,
+              ),
+            ]),
+          ],
+        ),
+        appBar: AppBar(
+          title: Text(title),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: "Why?",
+              ),
+              Tab(
+                text: "Chunking",
+              ),
+              Tab(
+                text: "Time management",
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
